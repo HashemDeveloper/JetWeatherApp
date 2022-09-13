16708469c7686f40bfd42609a721dbed1a9b5922
Abul Hashem Jr <abul@limosys.com>
2022-09-13 16:10:44 -0400

    Finish implementing about page

--- app/src/main/java/com/android/jetweather/screens/AboutScreen.kt
+++ app/src/main/java/com/android/jetweather/screens/AboutScreen.kt
@@ -1,10 +1,60 @@
 package com.android.jetweather.screens
 
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Scaffold
+import androidx.compose.material.Surface
 import androidx.compose.material.Text
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ArrowBack
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
 import androidx.navigation.NavHostController
+import com.android.jetweather.BuildConfig
+import com.android.jetweather.R
+import com.android.jetweather.components.WeatherTopBar
 
 @Composable
 fun AboutScreen(navController: NavHostController) {
-   Text(text = "About!")
+    Scaffold(topBar = {
+        WeatherTopBar(
+            title = stringResource(id = R.string.about),
+            icon = Icons.Default.ArrowBack,
+            navController = navController,
+            isMainScreen = false
+        ) {
+            navController.popBackStack()
+        }
+    }) {
+        if (BuildConfig.DEBUG) {
+            print(it)
+        }
+        Surface(
+            modifier = Modifier
+                .fillMaxWidth()
+                .fillMaxHeight()
+        ) {
+            Column(
+                horizontalAlignment = Alignment.CenterHorizontally,
+                verticalArrangement = Arrangement.Center
+            ) {
+                Text(
+                    text = stringResource(id = R.string.about_app),
+                    style = MaterialTheme.typography.subtitle1,
+                    fontWeight = FontWeight.Bold
+                )
+                Text(
+                    text = stringResource(id = R.string.api_used),
+                    style = MaterialTheme.typography.subtitle1,
+                    fontWeight = FontWeight.Bold
+                )
+            }
+        }
+    }
 }
